a1447de52e98efae91808969d4ceea900f5dd8ca
Sagar0155 <158021772+Sagar0155@users.noreply.github.com>
2024-03-31 16:13:04 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+let a=document.getElementById("home")
+let b=document.getElementById("about")
+let c=document.getElementById("portfi")
+let d=document.getElementById("serv")
+let e=document.getElementById("expi")
+let f=document.getElementById("con")
+let x=document.getElementById("hom")
+let y=document.getElementById("port")
+let z=document.getElementById("ser")
+let s=document.getElementById("expp")
+let t=document.getElementById("conta")
+
+a.addEventListener("click",()=>{
+    x.scrollIntoView({
+        behavior:"smooth"
+    })
+})
+b.addEventListener("click",()=>{
+    x.scrollIntoView({
+        behavior:"smooth"
+    })
+})
+c.addEventListener("click",()=>{
+    y.scrollIntoView({
+        behavior:"smooth"
+    })
+})
+d.addEventListener("click",()=>{
+    z.scrollIntoView({
+        behavior:"smooth"
+    })
+})
+e.addEventListener("click",()=>{
+    s.scrollIntoView({
+        behavior:"smooth"
+    })
+})
+f.addEventListener("click",()=>{
+    t.scrollIntoView({
+        behavior:"smooth"
+    })
+})
+let k=document.getElementById("ham")
+let l=document.getElementById("conta")
